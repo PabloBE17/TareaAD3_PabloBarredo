d6bdc66d9640945ee78555c4d3f0c2bfe11651f4
PabloBE17 <157827455+PabloBE17@users.noreply.github.com>
2025-02-28 19:01:50 +0100

    (8/9)Commits individuales por mal funcionamiento, todos ellos hacen las validaciones correctas para el buen funcionamiento del programa

--- src/main/java/com/luisdbb/tarea3AD2024base/repositorios/EnvioRepository.java
+++ src/main/java/com/luisdbb/tarea3AD2024base/repositorios/EnvioRepository.java
@@ -1,16 +1,49 @@
 package com.luisdbb.tarea3AD2024base.repositorios;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import com.luisdbb.tarea3AD2024base.config.ObjectDBManager;
 import com.luisdbb.tarea3AD2024base.modelo.EnvioACasa;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface EnvioRepository extends JpaRepository<EnvioACasa, Long> {
-    
-    List<EnvioACasa> findByIdParada(long idParada);
-    
-    List<EnvioACasa> findByUrgenteTrue();
-    List<EnvioACasa> findAll();
+public class EnvioRepository {
 
+    @Autowired
+    private ObjectDBManager dbManager;
+
+    public List<EnvioACasa> findByIdParada(long idParada) {
+        EntityManager em = dbManager.getEntityManager();
+        TypedQuery<EnvioACasa> query = em.createQuery("SELECT e FROM EnvioACasa e WHERE e.idParada = :idParada", EnvioACasa.class);
+        query.setParameter("idParada", idParada);
+        return query.getResultList();
+    }
+
+    public List<EnvioACasa> findByUrgenteTrue() {
+        EntityManager em = dbManager.getEntityManager();
+        TypedQuery<EnvioACasa> query = em.createQuery("SELECT e FROM EnvioACasa e WHERE e.urgente = true", EnvioACasa.class);
+        return query.getResultList();
+    }
+
+    public List<EnvioACasa> findAll() {
+        EntityManager em = dbManager.getEntityManager();
+        TypedQuery<EnvioACasa> query = em.createQuery("SELECT e FROM EnvioACasa e", EnvioACasa.class);
+        return query.getResultList();
+    }
+    
+    public void save(EnvioACasa envio) {
+        EntityManager em = dbManager.getEntityManager();
+        em.getTransaction().begin();
+        em.persist(envio);
+        em.getTransaction().commit();
+    }
+    
+    public void delete(EnvioACasa envio) {
+        EntityManager em = dbManager.getEntityManager();
+        em.getTransaction().begin();
+        em.remove(em.contains(envio) ? envio : em.merge(envio));
+        em.getTransaction().commit();
+    }
 }
